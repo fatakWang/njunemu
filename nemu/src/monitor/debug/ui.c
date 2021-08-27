3559c471fd589793e0122f0a05131fc6ce29cdc4
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 03:42:48 +0000

    >  compile NEMU 3020001267 simpleedu Linux simpleedu 4.15.0-154-generic #161-Ubuntu SMP Fri Jul 30 13:04:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:42:48 up 52 min,  1 user,  load average: 0.17, 0.21, 0.13 60b847af1918f8493db016dfb35c3acca5abca9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -101,14 +101,14 @@ i--;
 //calculate the first argument as n1
 
 //text the second argument start as 0x
-//char *endptr;
-/*uint32_t n2=strtoul(p1,&endptr,16);
+char *endptr;
+uint32_t n2=strtoul(p,&endptr,16);
 if(endptr!=NULL){
 printf("please enter a vaild argument\n");
 return 0;
-}*/
+}
 printf("%d\n",n1);
-printf("%s\n",p);
+printf("%u\n",n2);
 //calculate the second argument as ni2
 /*
 int i2=0;
